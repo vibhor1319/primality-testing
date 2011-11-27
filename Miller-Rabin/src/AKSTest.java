6c3e28325c46e09736f7b6ba54aa91b0e1b06cdb
vincentfu <vincentfu@localhost>
2011-11-27 11:39:29 -0700

    Added two 6-digit primes to test AKS prints out diagnostic info that will need to be removed--set verbose = false

--- Miller-Rabin/src/AKSTest.java
+++ Miller-Rabin/src/AKSTest.java
@@ -41,9 +41,15 @@ public class AKSTest
 		assertTrue(new AKS(BigInteger.valueOf(89)).isPrime());
 		assertFalse(new AKS(BigInteger.valueOf(90)).isPrime());
 		assertFalse(new AKS(BigInteger.valueOf(94)).isPrime());
+		assertFalse(new AKS(BigInteger.valueOf(131071-1)).isPrime());
+		assertTrue(new AKS(BigInteger.valueOf(131071)).isPrime());
+		assertFalse(new AKS(BigInteger.valueOf(131071+1)).isPrime());
+		assertFalse(new AKS(BigInteger.valueOf(524287-1)).isPrime());
+		assertTrue(new AKS(BigInteger.valueOf(524287)).isPrime());
+		assertFalse(new AKS(BigInteger.valueOf(524287+1)).isPrime());
 
 		// list from http://primes.utm.edu/lists/small/1000.txt
-		int [] primes = {
+		int [] primes1k = {
 	    2,     3,    5,    7,   11,   13,   17,   19,   23,   29, 
 	    31,   37,   41,   43,   47,   53,   59,   61,   67,   71, 
 	    73,   79,   83,   89,   97,  101,  103,  107,  109,  113, 
@@ -145,11 +151,11 @@ public class AKSTest
 	  7727, 7741, 7753, 7757, 7759, 7789, 7793, 7817, 7823, 7829, 
 	  7841, 7853, 7867, 7873, 7877, 7879, 7883, 7901, 7907, 7919 };
 		
-		for( int i = 2; i < primes.length; i++ )
+		for( int i = 2; i < primes1k.length; i++ )
 		{
-			assertFalse(new AKS(BigInteger.valueOf(primes[i]-1)).isPrime());
-			assertTrue(new AKS(BigInteger.valueOf(primes[i])).isPrime());
-			assertFalse(new AKS(BigInteger.valueOf(primes[i]+1)).isPrime());
+			assertFalse(new AKS(BigInteger.valueOf(primes1k[i]-1)).isPrime());
+			assertTrue(new AKS(BigInteger.valueOf(primes1k[i])).isPrime());
+			assertFalse(new AKS(BigInteger.valueOf(primes1k[i]+1)).isPrime());
 		}
 
 		// This one takes too long
